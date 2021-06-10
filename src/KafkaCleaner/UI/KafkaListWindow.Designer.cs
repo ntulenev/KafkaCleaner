b49f8c80b60f628dab58cb5f4cfff905e445c525
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-10 20:46:42 +0300

    Add UI

---
+++ src/KafkaCleaner/UI/KafkaListWindow.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace KafkaCleaner.UI
+{
+    partial class KafkaListWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bLoadData = new System.Windows.Forms.Button();
+            this.cbList = new System.Windows.Forms.CheckedListBox();
+            this.bDeleteData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bLoadData
+            // 
+            this.bLoadData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bLoadData.Location = new System.Drawing.Point(12, 12);
+            this.bLoadData.Name = "bLoadData";
+            this.bLoadData.Size = new System.Drawing.Size(776, 39);
+            this.bLoadData.TabIndex = 0;
+            this.bLoadData.Text = "Load data";
+            this.bLoadData.UseVisualStyleBackColor = true;
+            this.bLoadData.Click += new System.EventHandler(this.bLoadData_Click);
+            // 
+            // cbList
+            // 
+            this.cbList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbList.FormattingEnabled = true;
+            this.cbList.Location = new System.Drawing.Point(12, 57);
+            this.cbList.Name = "cbList";
+            this.cbList.Size = new System.Drawing.Size(776, 328);
+            this.cbList.TabIndex = 1;
+            // 
+            // bDeleteData
+            // 
+            this.bDeleteData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bDeleteData.Location = new System.Drawing.Point(12, 399);
+            this.bDeleteData.Name = "bDeleteData";
+            this.bDeleteData.Size = new System.Drawing.Size(776, 39);
+            this.bDeleteData.TabIndex = 2;
+            this.bDeleteData.Text = "Delete data";
+            this.bDeleteData.UseVisualStyleBackColor = true;
+            this.bDeleteData.Click += new System.EventHandler(this.bDeleteData_Click);
+            // 
+            // KafkaListWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bDeleteData);
+            this.Controls.Add(this.cbList);
+            this.Controls.Add(this.bLoadData);
+            this.Name = "KafkaListWindow";
+            this.Text = "Kafka Topics Cleaner";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bLoadData;
+        private System.Windows.Forms.CheckedListBox cbList;
+        private System.Windows.Forms.Button bDeleteData;
+    }
+}
---
+++ src/KafkaCleaner/UI/KafkaListWindow.cs
@@ -0,0 +1,73 @@
+﻿using Abstractions;
+using KafkaCleanerApp;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace KafkaCleaner.UI
+{
+    public partial class KafkaListWindow : Form
+    {
+        public KafkaListWindow(IKafkaServiceClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+            InitializeComponent();
+        }
+
+        private void RefreshData()
+        {
+            try
+            {
+                var items = _client.RequestTopicsList();
+                cbList.DataSource = items;
+                cbList.DisplayMember = nameof(Topic.Name);
+            }
+            catch
+            {
+                MessageBox.Show("Error on loading data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            cbList.UncheckAllItems();
+        }
+
+        private void ChangeAvailability(bool isEnabled)
+        {
+            bLoadData.Enabled = isEnabled;
+            bDeleteData.Enabled = isEnabled;
+            cbList.Enabled = isEnabled;
+        }
+
+        private void bLoadData_Click(object sender, EventArgs e)
+        {
+            ChangeAvailability(false);
+            RefreshData();
+            ChangeAvailability(true);
+        }
+
+        private async void bDeleteData_Click(object sender, EventArgs e)
+        {
+            ChangeAvailability(false);
+
+            try
+            {
+                foreach (var item in cbList.CheckedItems.Cast<Topic>())
+                {
+                    await _client.DeleteTopicAsync(item);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error on deleting data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            RefreshData();
+            ChangeAvailability(true);
+        }
+
+        private readonly IKafkaServiceClient _client;
+    }
+}

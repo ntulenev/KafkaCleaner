bd6ac17b21030c8c43cdbb16a96ce27d89a2b650
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-10 20:46:01 +0300

    Add KafkaServiceClientConfiguration

---
+++ src/Logic/KafkaServiceClientConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public class KafkaServiceClientConfiguration
+    {
+        public List<string> BootstrapServers { get; set; } = default!;
+        public List<string> ReservedTopics { get; set; } = default!;
+        public TimeSpan MetadataTimeout { get; set; }
+    }
+}

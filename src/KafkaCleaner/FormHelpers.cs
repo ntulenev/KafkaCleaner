7989ca50bf19b9d0dbe85224afb7176e7dcc86f7
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-10 20:46:26 +0300

    Add FormHelpers

---
+++ src/KafkaCleaner/FormHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaCleanerApp
+{
+    public static class FormHelpers
+    {
+        public static void UncheckAllItems(this System.Windows.Forms.CheckedListBox clb)
+        {
+            while (clb.CheckedIndices.Count > 0)
+                clb.SetItemChecked(clb.CheckedIndices[0], false);
+        }
+    }
+}

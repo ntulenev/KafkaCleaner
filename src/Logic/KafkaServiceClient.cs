7c0457ec519390a7cee3171515e078c13d016560
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-10 20:46:09 +0300

    Add KafkaServiceClient

---
+++ src/Logic/KafkaServiceClient.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Confluent.Kafka;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Models;
+using Abstractions;
+
+namespace Logic
+{
+    public class KafkaServiceClient : IKafkaServiceClient
+    {
+        public KafkaServiceClient(ILogger<KafkaServiceClient> logger, IOptions<KafkaServiceClientConfiguration> config)
+        {
+            if (config is null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            if (logger is null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            _config = config.Value;
+
+            _logger = logger;
+
+            _kafkaConfig = new AdminClientConfig
+            {
+                BootstrapServers = string.Join(",", _config.BootstrapServers)
+            };
+
+            _logger.LogInformation("Instance created.");
+        }
+
+        public IEnumerable<Topic> RequestTopicsList()
+        {
+            try
+            {
+                using var adminClient = new AdminClientBuilder(_kafkaConfig).Build();
+                var metadata = adminClient.GetMetadata(_config.MetadataTimeout);
+                var topicsMetadata = metadata.Topics;
+
+                return metadata.Topics.Select(a => a.Topic)
+                                      .Where(x => !_config.ReservedTopics.Contains(x))
+                                      .OrderBy(x => x)
+                                      .Select(x => new Topic(x))
+                                      .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error on loading topics.");
+                throw;
+            }
+        }
+
+        public async Task DeleteTopicAsync(Topic topic)
+        {
+            _logger.LogInformation("Trying to remove topic {topic}.", topic.Name);
+
+            if (topic is null)
+            {
+                throw new ArgumentNullException(nameof(topic));
+            }
+
+            if (_config.ReservedTopics.Contains(topic.Name))
+            {
+                throw new InvalidOperationException($"Unable to delete reserved topic - {topic.Name}.");
+            }
+
+            try
+            {
+                using var adminClient = new AdminClientBuilder(_kafkaConfig).Build();
+                await adminClient.DeleteTopicsAsync(new[] { topic.Name });
+
+                _logger.LogInformation("Topic {topic} removed.", topic.Name);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error on removing topic {topic}", topic.Name);
+                throw;
+            }
+        }
+
+        private readonly KafkaServiceClientConfiguration _config;
+        private readonly AdminClientConfig _kafkaConfig;
+        private readonly ILogger<KafkaServiceClient> _logger;
+    }
+}

94f208e2d2efd5b0d312524ddcaac01f65eadafe
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-10 20:46:17 +0300

    Add IKafkaServiceClient

---
+++ src/Abstractions/IKafkaServiceClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Models;
+
+namespace Abstractions
+{
+    public interface IKafkaServiceClient
+    {
+        public IEnumerable<Topic> RequestTopicsList();
+
+        public Task DeleteTopicAsync(Topic topic);
+    }
+}

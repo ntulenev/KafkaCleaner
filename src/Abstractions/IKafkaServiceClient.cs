99967eb8a5e74d915f4e244be44f07064f774012
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-11 16:29:38 +0300

    Add IKafkaServiceClient doc

--- src/Abstractions/IKafkaServiceClient.cs
+++ src/Abstractions/IKafkaServiceClient.cs
@@ -5,10 +5,20 @@ using Models;
 
 namespace Abstractions
 {
+    /// <summary>
+    /// Represents communication with Apache Kafka.
+    /// </summary>
     public interface IKafkaServiceClient
     {
+        /// <summary>
+        /// Gets list of kafka tipics.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Topic> RequestTopicsList();
 
+        /// <summary>
+        /// Deletes kafka topic
+        /// </summary>
         public Task DeleteTopicAsync(Topic topic);
     }
 }

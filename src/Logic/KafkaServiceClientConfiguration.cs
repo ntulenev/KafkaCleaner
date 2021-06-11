6d47d54525ef31af627bfcb1188888863b8746c0
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-11 16:27:16 +0300

    Add configuration doc

--- src/Logic/KafkaServiceClientConfiguration.cs
+++ src/Logic/KafkaServiceClientConfiguration.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 
 namespace Logic
 {
+    /// <summary>
+    /// Configuration for <see cref="KafkaServiceClient"/>.
+    /// </summary>
     public class KafkaServiceClientConfiguration
     {
+        /// <summary>
+        /// List of bootstrap servers
+        /// </summary>
         public List<string> BootstrapServers { get; set; } = default!;
+
+        /// <summary>
+        /// List of reserved topics (Hided and unable to be deleted).
+        /// </summary>
         public List<string> ReservedTopics { get; set; } = default!;
+
+        /// <summary>
+        /// Timout for metadata request.
+        /// </summary>
         public TimeSpan MetadataTimeout { get; set; }
     }
 }

d4877a10ff28945b3802810ca0789fca04a02783
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-11 16:24:56 +0300

    Add UncheckAllItems validation

--- src/KafkaCleaner/Helpers.cs
+++ src/KafkaCleaner/Helpers.cs
@@ -1,11 +1,23 @@
-﻿namespace KafkaCleanerApp
+﻿using System;
+
+namespace KafkaCleanerApp
 {
     public static class Helpers
     {
+        /// <summary>
+        /// Uncheck all items in <see cref="UncheckAllItems(System.Windows.Forms.CheckedListBox)."/>
+        /// </summary>
         public static void UncheckAllItems(this System.Windows.Forms.CheckedListBox clb)
         {
+            if (clb is null)
+            {
+                throw new ArgumentNullException(nameof(clb));
+            }
+
             while (clb.CheckedIndices.Count > 0)
+            {
                 clb.SetItemChecked(clb.CheckedIndices[0], false);
+            }
         }
     }
 }

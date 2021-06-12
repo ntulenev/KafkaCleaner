95707435236a52ff65635568bccb665eaf280db6
Nikita Tulenev <n1tulenev@gmail.com>
2021-06-12 10:05:03 +0300

    Add UI logs

--- src/KafkaCleaner/UI/KafkaListWindow.cs
+++ src/KafkaCleaner/UI/KafkaListWindow.cs
@@ -3,6 +3,8 @@ using System.Data;
 using System.Linq;
 using System.Windows.Forms;
 
+using Microsoft.Extensions.Logging;
+
 using Abstractions;
 using KafkaCleanerApp;
 using Models;
@@ -11,22 +13,30 @@ namespace KafkaCleaner.UI
 {
     public partial class KafkaListWindow : Form
     {
-        public KafkaListWindow(IKafkaServiceClient client)
+        public KafkaListWindow(IKafkaServiceClient client, ILogger<KafkaListWindow> logger)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
             InitializeComponent();
+
+            _logger.LogDebug("Instance created.");
         }
 
         private void RefreshData()
         {
             try
             {
+                _logger.LogDebug("Requesting topics list.");
+
                 var items = _client.RequestTopicsList();
                 cbList.DataSource = items;
                 cbList.DisplayMember = nameof(Topic.Name);
             }
-            catch
+            catch (Exception ex)
             {
+                _logger.LogError(ex, "Error on loading data");
+
                 MessageBox.Show("Error on loading data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
@@ -35,23 +45,37 @@ namespace KafkaCleaner.UI
 
         private bool IsAnySelectedTopics()
         {
+            _logger.LogDebug("Validating topics count for processing.");
+
             if (cbList.CheckedItems.Count == 0)
             {
                 _ = MessageBox.Show("No topics selected", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                _logger.LogDebug("No topics for processing.");
+
                 return false;
             }
+
+            _logger.LogDebug("{Count} topic(s) for processing.", cbList.CheckedItems.Count);
 
             return true;
         }
 
         private bool IsUserConfirmDeletion()
         {
-            var dialog = MessageBox.Show($"Do you want to delete {cbList.CheckedItems.Count} topic(s)", "Delete confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            return dialog == DialogResult.Yes;
+            _logger.LogDebug("Checking user delete confirmation.");
+
+            var dialogResult = MessageBox.Show($"Do you want to delete {cbList.CheckedItems.Count} topic(s)", "Delete confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            _logger.LogDebug("User reaction {Reaction}.", dialogResult);
+
+            return dialogResult == DialogResult.Yes;
         }
 
         private void ChangeAvailability(bool isEnabled)
         {
+            _logger.LogDebug("Changing UI availability to {status}.", isEnabled);
+
             bLoadData.Enabled = isEnabled;
             bDeleteData.Enabled = isEnabled;
             cbList.Enabled = isEnabled;
@@ -74,11 +98,15 @@ namespace KafkaCleaner.UI
                 {
                     foreach (var item in cbList.CheckedItems.Cast<Topic>())
                     {
+                        _logger.LogDebug("Trying to delete topic {topic}", item);
+
                         await _client.DeleteTopicAsync(item);
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
+                    _logger.LogError(ex, "Error on deleting data");
+
                     MessageBox.Show("Error on deleting data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
@@ -89,5 +117,6 @@ namespace KafkaCleaner.UI
         }
 
         private readonly IKafkaServiceClient _client;
+        private readonly ILogger<KafkaListWindow> _logger;
     }
 }

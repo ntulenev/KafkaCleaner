c12b4fc6c9fb5b5d5760f19b0946865874ff06a9
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-23 09:27:56 +0300

    Add top-level statements

--- src/KafkaCleaner/Program.cs
+++ src/KafkaCleaner/Program.cs
@@ -9,49 +9,42 @@ using Abstractions;
 using KafkaCleaner.UI;
 using Logic;
 
+// It is unfortunate but we have to set it to Unknown first.
+Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
+Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
 
-namespace KafkaCleanerApp
+Application.SetHighDpiMode(HighDpiMode.SystemAware);
+Application.EnableVisualStyles();
+Application.SetCompatibleTextRenderingDefault(false);
+
+var builder = new HostBuilder()
+  .ConfigureAppConfiguration((hostingContext, config) =>
+  {
+      config.AddJsonFile("appsettings.json", optional: true);
+  })
+  .ConfigureServices((hostContext, services) =>
+  {
+      services.AddScoped<KafkaListWindow>();
+      services.AddSingleton<IKafkaServiceClient, KafkaServiceClient>();
+      services.Configure<KafkaServiceClientConfiguration>(hostContext.Configuration.GetSection(nameof(KafkaServiceClientConfiguration)));
+
+      var logger = new LoggerConfiguration()
+                       .ReadFrom.Configuration(hostContext.Configuration)
+                       .CreateLogger();
+
+      services.AddLogging(x =>
+      {
+          x.SetMinimumLevel(LogLevel.Information);
+          x.AddSerilog(logger: logger, dispose: true);
+      });
+  });
+
+var host = builder.Build();
+
+using (var serviceScope = host.Services.CreateScope())
 {
-    static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+    var services = serviceScope.ServiceProvider;
 
-            var builder = new HostBuilder()
-              .ConfigureAppConfiguration((hostingContext, config) =>
-              {
-                  config.AddJsonFile("appsettings.json", optional: true);
-              })
-              .ConfigureServices((hostContext, services) =>
-              {
-                  services.AddScoped<KafkaListWindow>();
-                  services.AddSingleton<IKafkaServiceClient, KafkaServiceClient>();
-                  services.Configure<KafkaServiceClientConfiguration>(hostContext.Configuration.GetSection(nameof(KafkaServiceClientConfiguration)));
-
-                  var logger = new LoggerConfiguration()
-                                   .ReadFrom.Configuration(hostContext.Configuration)
-                                   .CreateLogger();
-
-                  services.AddLogging(x =>
-                  {
-                      x.SetMinimumLevel(LogLevel.Information);
-                      x.AddSerilog(logger: logger, dispose: true);
-                  });
-              });
-
-            var host = builder.Build();
-
-            using (var serviceScope = host.Services.CreateScope())
-            {
-                var services = serviceScope.ServiceProvider;
-
-                var form = services.GetRequiredService<KafkaListWindow>();
-                Application.Run(form);
-            }
-        }
-    }
+    var form = services.GetRequiredService<KafkaListWindow>();
+    Application.Run(form);
 }

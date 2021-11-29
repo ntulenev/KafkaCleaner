7084808c44db57991d84313ac9d7ccca5075f30a
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-29 09:36:42 +0300

    Add empty config validator

---
+++ src/Logic/Configuration/Validation/KafkaServiceClientConfigurationValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+using Microsoft.Extensions.Options;
+
+namespace Logic.Configuration.Validation
+{
+    /// <summary>
+    /// Validator for <see cref="KafkaServiceClientConfiguration"/>.
+    /// </summary>
+    public class KafkaServiceClientConfigurationValidator : IValidateOptions<KafkaServiceClientConfiguration>
+    {
+        /// <summary>
+        /// Validates <see cref="KafkaServiceClientConfiguration"/>.
+        /// </summary>
+        public ValidateOptionsResult Validate(string name, KafkaServiceClientConfiguration options)
+        {
+            Debug.Assert(name is not null);
+            Debug.Assert(options is not null);
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}

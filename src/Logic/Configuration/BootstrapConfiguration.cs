ef272bd03048746ab2ac347c36a5bfc72e913d63
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-29 16:21:34 +0300

    Add BootstrapConfiguration

---
+++ src/Logic/Configuration/BootstrapConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Logic.Configuration
+{
+    /// <summary>
+    /// bootstrap servers config
+    /// </summary>
+    public class BootstrapConfiguration
+    {
+        /// <summary>
+        /// List of bootstrap servers
+        /// </summary>
+        public List<string> BootstrapServers { get; set; } = default!;
+    }
+}

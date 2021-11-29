b42f6ab4f880fec78a54659fc8978c2b9dddc865
Nikita Tulenev <n1tulenev@gmail.com>
2021-11-29 16:22:48 +0300

    Add AdminClientBuilder in DI

--- src/KafkaCleaner/Program.cs
+++ src/KafkaCleaner/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -5,6 +7,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 using Serilog;
+
+using Confluent.Kafka;
 
 using Abstractions;
 using KafkaCleaner.UI;
@@ -29,8 +33,34 @@ var builder = new HostBuilder()
   {
       services.AddScoped<KafkaListWindow>();
       services.AddSingleton<IKafkaServiceClient, KafkaServiceClient>();
+      services.AddSingleton(s =>
+      {
+          var section = hostContext.Configuration.GetSection(nameof(BootstrapConfiguration));
+          var config = section.Get<BootstrapConfiguration>();
+
+          Debug.Assert(config is not null);
+
+          var validator = s.GetRequiredService<IValidateOptions<BootstrapConfiguration>>();
+
+          // Crutch to use IValidateOptions in manual generation logic.
+          var validationResult = validator.Validate(string.Empty, config);
+          if (validationResult.Failed)
+          {
+              throw new OptionsValidationException
+                  (string.Empty, typeof(BootstrapConfiguration), new[] { validationResult.FailureMessage });
+          }
+
+          var adminConfig = new AdminClientConfig
+          {
+              BootstrapServers = string.Join(",", config.BootstrapServers)
+          };
+
+          return new AdminClientBuilder(adminConfig).Build();
+
+      });
       services.Configure<KafkaServiceClientConfiguration>(hostContext.Configuration.GetSection(nameof(KafkaServiceClientConfiguration)));
-      services.AddSingleton<IValidateOptions<KafkaServiceClientConfiguration>, KafkaServiceClientConfigurationValidator>();
+      services.Configure<BootstrapConfiguration>(hostContext.Configuration.GetSection(nameof(BootstrapConfiguration)));
+      services.AddSingleton<IValidateOptions<BootstrapConfiguration>, BootstrapConfigurationValidator>();
 
       var logger = new LoggerConfiguration()
                        .ReadFrom.Configuration(hostContext.Configuration)
@@ -49,6 +79,13 @@ using (var serviceScope = host.Services.CreateScope())
 {
     var services = serviceScope.ServiceProvider;
 
-    var form = services.GetRequiredService<KafkaListWindow>();
-    Application.Run(form);
+    try
+    {
+        var form = services.GetRequiredService<KafkaListWindow>();
+        Application.Run(form);
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.Message, "Application error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
 }

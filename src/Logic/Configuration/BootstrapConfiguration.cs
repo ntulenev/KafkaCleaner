2d740e774b42592d417f0ed42048e5e8b433f60c
Nikita Tulenev <n1tulenev@gmail.com>
2021-12-13 21:20:38 +0300

    Extend BootstrapConfiguration with security settings

--- src/Logic/Configuration/BootstrapConfiguration.cs
+++ src/Logic/Configuration/BootstrapConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace Logic.Configuration
+﻿using Confluent.Kafka;
+
+namespace Logic.Configuration
 {
     /// <summary>
     /// bootstrap servers config
@@ -9,5 +11,25 @@
         /// List of bootstrap servers
         /// </summary>
         public List<string> BootstrapServers { get; set; } = default!;
+
+        /// <summary>
+        /// Kafka user name, is any.
+        /// </summary>
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// Kafka password name, is any.
+        /// </summary>
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// Kafka security protocol.
+        /// </summary>
+        public SecurityProtocol SecurityProtocol { get; set; } = SecurityProtocol.Plaintext;
+
+        /// <summary>
+        /// Kafka security protocol mechanism.
+        /// </summary>
+        public SaslMechanism? SASLMechanism { get; set; }
     }
 }
